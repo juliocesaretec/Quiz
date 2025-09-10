1754ac92a2b5fd428afb0c2b8f8550a80da6dba2
juliocesaretec <juliocpsjr7@gmail.com>
2025-09-10 17:00:30 -0300

    Primeiro envio

---
+++ src/Components/Quiz/Quiz.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Quiz.css'
+
+const Quiz = () => {
+
+        let [index,setIndex] = useState(0);
+    
+    return (
+        <div className='container'>
+            <h1>Quiz App</h1>
+            <hr />
+            <h2>1. Which device is required for the internet connection?</h2>
+            <ul>
+                <li>Modem</li>
+                <li>Router</li>
+                <li>LAN Cable</li>
+                <li>Pen Drive</li>
+            </ul>
+            <button>Next</button>
+            <div className="index">1 of 5 questions</div>
+        </div>
+    )
+}
+
+export default Quiz
